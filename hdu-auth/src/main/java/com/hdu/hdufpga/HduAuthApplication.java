f4fd6088ad39e1f071e9635b110dd4eae9904858
哲学受 <mac@macdeMacBook-Pro.local>
2025-11-09 15:42:24 +0800

    cors

--- hdu-auth/src/main/java/com/hdu/hdufpga/HduAuthApplication.java
+++ hdu-auth/src/main/java/com/hdu/hdufpga/HduAuthApplication.java
@@ -1,14 +1,17 @@
 package com.hdu.hdufpga;
 
+import com.hdu.hdufpga.config.WebConfiguration;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@Import(WebConfiguration.class)
+@SpringBootApplication(scanBasePackages = "com.hdu")
 @EnableDubbo
 public class HduAuthApplication {
     public static void main(String[] args) {
