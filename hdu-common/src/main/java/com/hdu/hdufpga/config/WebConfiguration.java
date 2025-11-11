129238373c5a5be15b00872edd7a5bb28d7502c0
Zhe-xue-shou <3311356727@qq.com>
2025-11-11 15:27:28 +0800

    fix WebConfiguration bug

--- hdu-common/src/main/java/com/hdu/hdufpga/config/WebConfiguration.java
+++ hdu-common/src/main/java/com/hdu/hdufpga/config/WebConfiguration.java
@@ -5,18 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
   @Value("${cors.allowed-origins}")
-  private List<String> allowedOrigins;
+  private String[] allowedOrigins;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins(String.valueOf(allowedOrigins))
+        .allowedOrigins(allowedOrigins)
         .allowedMethods("POST", "GET", "OPTIONS", "DELETE", "PUT")
         .allowedHeaders("x-requested-with", "satoken", "Content-Type", "Authorization", "token")
         .allowCredentials(true);
