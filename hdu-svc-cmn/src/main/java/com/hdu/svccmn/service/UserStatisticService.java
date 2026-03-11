b8fc9bc3c06a7eba4a90a350f29cf6035051922c
Zhe-xue-shou <3311356727@qq.com>
2026-03-11 20:37:41 +0800

    refactor: 重构了UserStatisticService模块

--- hdu-svc-cmn/src/main/java/com/hdu/svccmn/service/UserStatisticService.java
+++ hdu-svc-cmn/src/main/java/com/hdu/svccmn/service/UserStatisticService.java
@@ -1,10 +1,5 @@
 package com.hdu.svccmn.service;
 
-import com.hdu.hdufpga.entity.vo.UserVO;
-
 public interface UserStatisticService {
-  void storeUserByToken(String token, UserVO userVO);
-
-  void updateUserExptime(String token, Long sTime);
-
+  void updateUserExptime(String username, Integer departmentId, Long sTime) throws Exception;
 }
--- hdu-svc-cmn/src/main/java/com/hdu/svccmn/service/impl/UserStatisticServiceImpl.java
+++ hdu-svc-cmn/src/main/java/com/hdu/svccmn/service/impl/UserStatisticServiceImpl.java
@@ -1,18 +1,14 @@
 package com.hdu.svccmn.service.impl;
 
 import com.hdu.hdufpga.entity.po.UserPO;
-import com.hdu.hdufpga.entity.vo.UserVO;
 import com.hdu.hdufpga.service.UserService;
 import com.hdu.svccmn.service.UserStatisticService;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Nullable;
 import java.time.Duration;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @Slf4j
@@ -20,42 +16,24 @@ public class UserStatisticServiceImpl implements UserStatisticService {
   @DubboReference(check = false)
   private UserService userService;
 
-  private final ConcurrentHashMap<String, UserVO> tokTable = new ConcurrentHashMap<>();
-
   @Override
   @Transactional
-  public void updateUserExptime(@NonNull String token, @Nullable Long sTime) {
-    UserVO userVO = tokTable.remove(token); // remove the token from the table
-
-    if (userVO == null) {
-      log.error("an unexpect error has occurred: user with token {} is null", token);
-      return;
-    }
+  public void updateUserExptime(String username, Integer departmentId, Long sTime) throws Exception {
     long curTime = System.currentTimeMillis();
 
     if (sTime == null || sTime <= 0 || sTime >= curTime) {
-      log.error("an unexpect error has occurred: user with token {} has no exp time", token);
-      return;
+      throw new Exception("stime is null or stime > curtime");
     }
     Duration expTime = Duration.ofMillis(curTime - sTime);
-
-    // todo: is it needed to also modify the UserVO?
-    Duration currentTotalTime = userVO.getTotActiveTime();
-    userVO.setTotActiveTime(currentTotalTime.plus(expTime));
-
-    int currentExpCount = userVO.getTotExpCnt();
-    userVO.setTotExpCnt(currentExpCount + 1);
 
     long expTimeInMillis = expTime.toMillis();
 
     // get UserPO by username and department ID
-    UserPO userPO = userService.getUserByUserName(
-        userVO.getUsername(),
-        userVO.getUserDepartmentId());
+    UserPO userPO = userService.getUserByUserName(username, departmentId);
 
     if (userPO == null) {
       log.error("Cannot find userPO in database for username: {}, departmentId: {}",
-          userVO.getUsername(), userVO.getUserDepartmentId());
+          username, departmentId);
       return;
     }
 
@@ -67,11 +45,6 @@ public class UserStatisticServiceImpl implements UserStatisticService {
 
     userService.updateById(userPO);
     log.info("Updated user {} statistics: +{} ms, +1 exp",
-        userVO.getUsername(), expTimeInMillis);
-  }
-
-  @Override
-  public void storeUserByToken(String token, UserVO userVO) {
-    tokTable.put(token, userVO);
+        username, expTimeInMillis);
   }
 }
